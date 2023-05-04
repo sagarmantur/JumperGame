114b6f9c3fa16c2367e49567832aa7f21a50a895
Sagar <sagar.pheuna@gmail.com>
2023-05-05 00:20:08 +0530

    Uplaoding Game files

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1683225643,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/ground_stone-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
